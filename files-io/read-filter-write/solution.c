0d4edeeb6d3cfa50b1ab57a0c89b39c704f217f8
artem <arta90633@gmail.com>
2024-12-24 19:20:54 +0500

    read-filter-write fixed

--- files-io/read-filter-write/solution.c
+++ files-io/read-filter-write/solution.c
@@ -3,36 +3,19 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <ctype.h>
+#include <errno.h>
 
 #define MAX_BUFFER_SIZE 4096
 
 int main(int argc, char *argv[]) {
-  if (argc < 4) {
-    perror("Too few arguments error");
+  if (argc != 4) {
+    perror("Arguments count error");
     return 1;
   }
   int input_fd = open(argv[1], O_RDONLY);
   if (input_fd < 0) {
     perror("Error opening input file");
     return 1;
-  }
-  char digits_buffer[MAX_BUFFER_SIZE];
-  char other_buffer[MAX_BUFFER_SIZE];
-  size_t digits_index = 0;
-  size_t other_index = 0;
-  char buffer;
-  size_t bytes_read;
-  while ((bytes_read = read(input_fd, &buffer, 1)) > 0) {
-    if (isdigit(buffer)) {
-      digits_buffer[digits_index++] = buffer;
-    } else {
-      other_buffer[other_index++] = buffer;
-    }
-  }
-  if (bytes_read < 0) {
-    perror("Error reading input file");
-    close(input_fd);
-    return 3;
   }
   int output_fd_digits = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0644);
   if (output_fd_digits < 0) {
@@ -46,19 +29,32 @@ int main(int argc, char *argv[]) {
     close(input_fd);
     close(output_fd_digits);
     return 2;
-    }
-  if (write(output_fd_digits, digits_buffer, digits_index) < 0) {
-    perror("Error writing to digits file");
-    close(input_fd);
-    close(output_fd_digits);
-    close(output_fd_other);
-    return 3;
   }
-  if (write(output_fd_other, other_buffer, other_index) < 0) {
-    perror("Error writing to other characters file");
-    close(input_fd);
-    close(output_fd_digits);
-    close(output_fd_other);
+  char buffer[MAX_BUFFER_SIZE];
+  ssize_t bytes_read;
+  while ((bytes_read = read(input_fd, buffer, MAX_BUFFER_SIZE)) > 0) {
+    char digits_buffer[MAX_BUFFER_SIZE];
+    char other_buffer[MAX_BUFFER_SIZE];
+    ssize_t digits_index = 0;
+    ssize_t other_index = 0;
+    for (ssize_t i = 0; i < bytes_read; i++) {
+      if (isdigit(buffer[i])) {
+        digits_buffer[digits_index++] = buffer[i];
+      } else {
+        other_buffer[other_index++] = buffer[i];
+      }
+    }
+    if (write(output_fd_digits, digits_buffer, digits_index) < 0) {
+      perror("Error writing to digits file");
+      return 3;
+    }
+    if (write(output_fd_other, other_buffer, other_index) < 0) {
+      perror("Error writing to other characters file");
+      return 3;
+      }
+  }
+  if (bytes_read < 0) {
+    perror("Error reading input file");
     return 3;
   }
   close(input_fd);
